8cfac38052e387e37f821104c41bd0d297f0b2ed
hernan <hernan@hernan-HP-Laptop-14-bs0xx>
2020-05-30 00:57:17 -0300

    *asunto: inicializacion del proyecto. *responsable: Humana Hernan. *task00: creacion del proyecto spring boot del tp5

---
+++ src/main/java/ar/edu/unju/fi/Humana3884Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Humana3884Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Humana3884Tp5Application.class, args);
+	}
+
+}
